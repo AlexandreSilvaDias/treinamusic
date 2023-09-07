64947d55011de46175066cd3608d75c710a4be39
Alexandre <alexandre.silva.dias@outlook.com>
2023-09-07 14:18:51 -0300

    aula02 - lista e ordenacao

--- src/ui/pages/index.jsx
+++ src/ui/pages/index.jsx
@@ -1,3 +1,34 @@
+import MusicList from "../components/data.display/MusicList/MusicList"
+
+const musics = [
+    {
+        id: 1,
+        name: "Music 1",
+        artist: "Artist Alfa",
+        time: 90,
+    },
+    {
+        id: 2,
+        name: "Music 2",
+        artist: "Artist Bravo",
+        time: 95,
+    },
+    {
+        id: 3,
+        name: "Music 3",
+        artist: "Artist Charlie",
+        time: 100,
+    },
+];
+
 export default function Index() {
-    return <div>Index</div>
+    return (
+        <div>
+            <MusicList 
+            musics={musics} 
+            selectedMusic={musics[0]} 
+            onSelect={() => {}}
+            />
+        </div>
+    );
 }
